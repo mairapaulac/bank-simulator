45e8041f232b4c3e32b07a8e22c6f8c24de2437d
Maíra Paula <mairapaulac@hotmail.com>
2025-05-24 17:42:28 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,71 @@
+/*
+Atualizar o saldo ao clicar em "Depositar" ou "Sacar". ok
+
+Adicionar itens na lista de transações.
+
+Validar se o saque não ultrapassa o saldo. 
+
+Adicionar estilos dinâmicos (ex: cor do texto se for negativo).
+
+Armazenar dados em localStorage (opcional).
+*/
+
+
+var dinheiro = document.getElementById("amount");
+var saldoAtual = document.getElementById("balance");
+var rs = document.getElementById("currency");
+let listaTransacoes = [];
+
+function adicionarTransacao(valor) {
+  const lista = document.getElementById("transactions-list");
+
+  
+  listaTransacoes.push(valor);
+
+  
+  const ultimasTransacoes = listaTransacoes.slice(-4);
+
+  lista.innerHTML = "";
+
+ 
+  ultimasTransacoes.forEach(transacao => {
+    const item = document.createElement("li");
+    const tipo = transacao > 0 ? "Depósito" : "Saque";
+    const cor = transacao > 0 ? "green" : "red";
+
+    item.textContent = `${tipo}: R$ ${Math.abs(transacao).toFixed(2)}`;
+    item.style.color = cor;
+
+    lista.appendChild(item);
+  });
+}
+
+
+
+function deposito() {
+   var valorDep = parseFloat(dinheiro.value);
+   var saldo = parseFloat(saldoAtual.innerHTML);
+   var novoSaldo = valorDep + saldo;
+
+   saldoAtual.innerHTML = novoSaldo.toFixed(2);
+
+   adicionarTransacao(valorDep);
+}
+
+function saque() {
+    var valorSaque = parseFloat(dinheiro.value);
+    var saldo = parseFloat(saldoAtual.innerHTML);
+    var novoSaldo = saldo - valorSaque;
+
+    saldoAtual.innerHTML = novoSaldo.toFixed(2);
+
+    if (isNaN(valorSaque) || valorSaque <= 0) return;
+
+    if (valorSaque > saldo)   {
+        alert("Saldo insuficiente para o saque!");
+        saldoAtual.style.color = "red";
+        rs.style.color = "red";
+    }
+
+    adicionarTransacao(-valorSaque);
+}
